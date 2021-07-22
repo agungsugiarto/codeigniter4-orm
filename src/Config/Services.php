86505456f076a237e9f2e0e53b1a67b2a2902766
agungsugiarto <me.agungsugiarto@gmail.com>
2021-07-22 16:00:12 +0700

    WIP: CodeIgniter4 Orm

---
+++ src/Config/Services.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Fluent\Orm\Config;
+
+use CodeIgniter\Config\BaseService;
+use Faker\Factory as FakerFactory;
+use Fluent\Orm\Factory as OrmFactory;
+
+class Services extends BaseService
+{
+    /**
+     * The array of resolved Faker instances.
+     *
+     * @var array
+     */
+    protected static $fakers = [];
+
+    /**
+     * Service faker instance.
+     * 
+     * @param string $locale
+     * @param bool $getShared
+     * @return \Faker\Generator
+     */
+    public static function faker($locale = 'en_US', bool $getShared = true)
+    {
+        if ($getShared) {
+            return static::getSharedInstance('faker', $locale);
+        }
+
+        if (! isset(static::$fakers[$locale])) {
+            static::$fakers[$locale] = FakerFactory::create($locale);
+        }
+
+        static::$fakers[$locale]->unique(true);
+
+        return static::$fakers[$locale];
+    }
+
+    /**
+     * Service orm factory instance.
+     * 
+     * @param string $pathToFactories
+     * @param string $locale
+     * @param bool $getShared
+     * @return OrmFactory
+     */
+    public static function factory($pathToFactories = APPPATH . 'Database/Factories', $locale = 'en_US', bool $getShared = true)
+    {
+        if ($getShared) {
+            return static::getSharedInstance('factory', $pathToFactories, $locale);
+        }
+
+        return OrmFactory::construct(static::faker($locale, $getShared), $pathToFactories);
+    }
+}
--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -12,3 +12,24 @@ if (! function_exists('last')) {
         return end($array);
     }
 }
+
+if (! function_exists('factory')) {
+    /**
+     * Create a model factory builder for a given class and amount.
+     *
+     * @param  string  $class
+     * @param  int  $amount
+     * @return \Fluent\Orm\FactoryBuilder
+     */
+    function factory($class, $amount = null)
+    {
+        /** @var \Fluent\Orm\Factory $factory */
+        $factory = service('factory');
+
+        if (isset($amount) && is_int($amount)) {
+            return $factory->of($class)->times($amount);
+        }
+
+        return $factory->of($class);
+    }
+}
